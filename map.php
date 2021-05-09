3a79fbcd3f14623fcec5e69021c7b11d94dc69b0
ranusingh123 <83719051+ranusingh123@users.noreply.github.com>
2021-05-09 19:23:03 +0530

    Update map.php

--- map.php
+++ map.php
@@ -1,32 +1,26 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<style type="text/css">
-		*{
-			margin: 0;
-			padding: 0;
-		}
-		#map{
-			height: 500px;
-			width: 100%;
-		}
-	</style>
-</head>
-<body>
-<div id="map"></div>
-	
-</script>
-<script type="text/javascript">
-	function initMap(){
-		var location={lat:-25.363,lnt:131.044};
-		var map= new google.maps.Map(document.getElementById("map"),{
-			zoom:4,
-			center: location
-		});
-	}
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script>
+    var myMap;
+    var myLatlng = new google.maps.LatLng(18.5204,73.8567);
+    var myLatlngsiwan = new google.maps.LatLng(26.2243,84.3600);
+    function initialize() {
+        var mapOptions = {
+            zoom: 13,
+            center: myLatlngsiwan,
+            mapTypeId: google.maps.MapTypeId.ROADMAP  ,
+            scrollwheel: false
+        }
+        myMap = new google.maps.Map(document.getElementById('map'), mapOptions);
+        var marker = new google.maps.Marker({
+            position: myLatlngsiwan,
+            map: myMap,
+            title: 'Name Of Business',
+            icon: 'http://www.google.com/intl/en_us/mapfiles/ms/micons/red-dot.png'
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
 </script>
 
-</body>
-</html>
+<div id="map" style="width:500px; height: 500px;">
+
+</div>
